b0de7c9dd44d1a303fe7c287b4a5fe7b23ec8e4f
Jörg Thalheim <joerg@thalheim.io>
2018-06-28 11:45:30 +0100

    make-release.sh: actually tag release there

--- make-release.sh
+++ make-release.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p git -i bash
+#!nix-shell -p git -p nix -p gitAndTools.hub -i bash
 
+set -eux -o pipefail
+
+version=$(nix eval --raw '(import ./nix/release.nix {}).tarball.version')
+git checkout master
+git tag -d "$version" || true
+git tag "$version"
 git push --tags
